5396cea5fde71e81ba2b7df1f09053523b879ccc
Smirnov Vadim <badimm.235@gmail.com>
2014-09-22 14:28:06 +0700

    Add project files before using pod install.
    Add project files before using pod install.

---
+++ TestAssignment/TAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TAAppDelegate.h
+//  TestAssignment
+//
+//  Created by Wadim on 22/09/14.
+//  Copyright (c) 2014 Smirnov Electronics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
